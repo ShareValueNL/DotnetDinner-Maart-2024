56007386f0accca0c713163025ac60ca07072d4d
DevByRayRay <raymon@byrayray.dev>
2024-03-07 21:05:06 +0100

    Added design and promts

--- my-app/src/app/api.service.ts
+++ my-app/src/app/api.service.ts
@@ -4,12 +4,12 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:8081'; // Replace with your API URL
+  private apiUrl = 'http://localhost:5085'; // Replace with your API URL
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getWinners(): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/`);
--- my-app/src/app/winners/winners.component.ts
+++ my-app/src/app/winners/winners.component.ts
@@ -5,16 +5,18 @@ import { ApiService } from '../api.service';
 @Component({
   selector: 'app-winners',
   templateUrl: './winners.component.html',
-  styleUrls: ['./winners.component.scss']
+  styleUrls: ['./winners.component.scss'],
 })
 export class WinnersComponent implements OnInit {
-  winners: any;
+  prizes: string[] = [];
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  loadWinners(): void {
     this.apiService.getWinners().subscribe((data) => {
-      this.winners = data;
+      this.prizes = [data.firstPrize, data.secondPrize];
     });
   }
 }
